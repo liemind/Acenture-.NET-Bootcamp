bc036c370ea723945714d88a27b5d24d57459b0b
Daniela Paredes <daredes.r@hotmail.com>
2021-05-26 14:42:51 -0400

    Init File

---
+++ Project/Salvo/Salvo/Models/SalvoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Salvo.Models
+{
+    public class SalvoContext : DbContext
+    {
+        public SalvoContext(DbContextOptions<SalvoContext> options) : base(options)
+        {
+        }
+
+        //Items created in Model. They will register here.
+        public DbSet<SalvoItem> SalvoItems { get; set; }
+    }
+}
