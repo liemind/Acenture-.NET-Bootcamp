47fffd1c8fa03e12b9852c07c1e274fba38ef686
Daniela Paredes <daredes.r@hotmail.com>
2021-05-27 15:55:43 -0400

    Clean files

--- Project/Salvo/Salvo/Models/Data/DbInitializer.cs
+++ Project/Salvo/Salvo/Models/Data/DbInitializer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Salvo.Models;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace Salvo.Models.Data
 {
--- Project/Salvo/Salvo/Program.cs
+++ Project/Salvo/Salvo/Program.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Salvo.Models.Data;
-using Salvo.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Salvo.Models;
+using Salvo.Models.Data;
+using System;
 
 namespace Salvo
 {
