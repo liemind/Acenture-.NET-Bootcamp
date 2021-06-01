c12989bedbc6236132f55f4328d38dcf5d58611a
Daniela Paredes <daredes.r@hotmail.com>
2021-06-01 18:03:52 -0400

    Add Score Class

--- Project/Salvo/Salvo/Models/Score.cs
+++ Project/Salvo/Salvo/Models/Score.cs
@@ -1,27 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Salvo.Models
 {
-    public class GamePlayer
+    public class Score
     {
         [Key]
         public long Id { get; set; }
-
-        [Required]
-        public DateTime JoinDate { get; set; }
+        public double Point { get; set; }
+        public DateTime FinishDate { get; set; }
         public long GameId { get; set; }
         [ForeignKey("GameId")]
         public Game Game { get; set; }
         public long PlayerId { get; set; }
         [ForeignKey("PlayerId")]
         public Player Player { get; set; }
-
-        public ICollection<Ship> Ships { get; set; }
-        public ICollection<Salvo> Salvos { get; set; }
-        public ICollection<Score> Scores { get; set; }
 
     }
 }
