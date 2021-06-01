935ad2134b7e6a6656028426f828fc583d39d0dd
Daniela Paredes <daredes.r@hotmail.com>
2021-06-01 15:40:16 -0400

    Add DTO

---
+++ Project/Salvo/Salvo/Models/DTO/SalvoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Salvo.Models.DTO
+{
+    public class SalvoDTO
+    {
+        public long Id { get; set; }
+        public int Turn { get; set; }
+        public PlayerDTO player { get; set; }
+    }
+}
